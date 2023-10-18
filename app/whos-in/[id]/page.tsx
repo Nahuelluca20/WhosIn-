bce57fd02df7065965eae21535fc294d41c0269b
Nahuel Luca <nahuelluca20@gmail.com>
2023-10-18 17:18:27 -0300

    add: map user attend event

--- app/whos-in/[id]/page.tsx
+++ app/whos-in/[id]/page.tsx
@@ -9,27 +9,52 @@ import {Calendar} from "@/components/ui/calendar";
 export default async function page({params}: {params: {id: string}}) {
   const eventData: any = await getEventById(params.id as string);
 
-  // console.log(eventData.data[0]);
+  console.log(eventData.data[0]?.data.users_attend);
   let totalGuests = eventData.data[0]?.data.total_guests;
+  let usersAttend = eventData.data[0]?.data.users_attend;
+  let unconfirmed = eventData.data[0]?.data.unconfirmed;
+  let attend = eventData.data[0]?.data.attend;
   let eventTitle = eventData.data[0]?.data.event_title;
   let placeDirection = eventData.data[0]?.data.place_direction;
+  let placeName = eventData.data[0]?.data.place_name;
   let dayFormat = new Date(eventData.data[0]?.data.event_date);
 
   return (
-    <main className="w-full flex gap-5 justify-center px-5 md:px-10">
+    <main className="w-full grid pb-5 md:flex gap-5 justify-center px-5 md:px-10">
       <Card className="col-span-3 w-full max-w-[750px]">
         <CardHeader className="">
           <CardTitle className="text-lg font-bold mb-[-6px]">Datos del evento</CardTitle>
           <CardDescription>{eventTitle}</CardDescription>
         </CardHeader>
         <CardContent className="grid gap-2">
-          <div className="w-fit">
-            <Calendar className="rounded-md border" mode="single" selected={dayFormat} />
-          </div>
-
-          <Link href={placeDirection} target="_blank">
-            <Button className="text-white">Ver dirección</Button>
-          </Link>
+          <section className="grid xl:flex gap-5">
+            <div className="w-fit mx-auto xl:mx-0">
+              <Calendar className="rounded-md border" mode="single" selected={dayFormat} />
+            </div>
+            <aside className="grid content-between justify-center gap-2">
+              <div className="grid grid-cols-2 content-start gap-x-6 gap-y-8">
+                <div>
+                  <span className="text-sm font-bold">Nombre del lugar:</span>
+                  <p className="text-sm">{placeName}</p>
+                </div>
+                <div>
+                  <span className="text-sm font-bold">Personas invitadas:</span>
+                  <p className="text-sm">{attend}</p>
+                </div>
+                <div>
+                  <span className="text-sm font-bold">Asisten:</span>
+                  <p className="text-sm">{totalGuests}</p>
+                </div>
+                <div>
+                  <span className="text-sm font-bold">Sin confirmar:</span>
+                  <p className="text-sm">{unconfirmed}</p>
+                </div>
+              </div>
+              <Link href={placeDirection} target="_blank">
+                <Button className="text-white">Ver dirección</Button>
+              </Link>
+            </aside>
+          </section>
         </CardContent>
       </Card>
       <Card className="col-span-3 w-full max-w-[450px]">
@@ -38,7 +63,7 @@ export default async function page({params}: {params: {id: string}}) {
           <CardDescription>Hay {totalGuests} personas invitadas.</CardDescription>
         </CardHeader>
         <CardContent>
-          <GuestUsers />
+          <GuestUsers usersAttend={usersAttend} />
         </CardContent>
       </Card>
     </main>
--- components/cards/guest-users.tsx
+++ components/cards/guest-users.tsx
@@ -1,63 +1,38 @@
 import {Avatar, AvatarImage, AvatarFallback} from "../ui/avatar";
 
-export default function GuestUsers() {
+function getTwoFirstLetters(text: string) {
+  return text
+    .split(" ")
+    .map((word) => word[0].toLocaleUpperCase())
+    .slice(0, 2)
+    .join("");
+}
+
+export default function GuestUsers({
+  usersAttend,
+}: {
+  usersAttend: {
+    name: string;
+    email: string;
+  }[];
+}) {
+  console.log(getTwoFirstLetters("tata ramos"));
+
   return (
     <div className="space-y-8">
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage alt="Avatar" src="/avatars/01.png" />
-          <AvatarFallback>OM</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Olivia Martin</p>
-          <p className="text-sm text-muted-foreground">olivia.martin@email.com</p>
-        </div>
-        <div className="ml-auto font-medium">+$1,999.00</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="flex h-9 w-9 items-center justify-center space-y-0 border">
-          <AvatarImage alt="Avatar" src="/avatars/02.png" />
-          <AvatarFallback>JL</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Jackson Lee</p>
-          <p className="text-sm text-muted-foreground">jackson.lee@email.com</p>
-        </div>
-        <div className="ml-auto font-medium">+$39.00</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage alt="Avatar" src="/avatars/03.png" />
-          <AvatarFallback>IN</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Isabella Nguyen</p>
-          <p className="text-sm text-muted-foreground">isabella.nguyen@email.com</p>
-        </div>
-        <div className="ml-auto font-medium">+$299.00</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage alt="Avatar" src="/avatars/04.png" />
-          <AvatarFallback>WK</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">William Kim</p>
-          <p className="text-sm text-muted-foreground">will@email.com</p>
-        </div>
-        <div className="ml-auto font-medium">+$99.00</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage alt="Avatar" src="/avatars/05.png" />
-          <AvatarFallback>SD</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Sofia Davis</p>
-          <p className="text-sm text-muted-foreground">sofia.davis@email.com</p>
-        </div>
-        <div className="ml-auto font-medium">+$39.00</div>
-      </div>
+      {usersAttend &&
+        usersAttend.map((user, index) => (
+          <div key={index} className="flex items-center">
+            <Avatar className="h-9 w-9">
+              <AvatarImage alt="Avatar" src="/avatars/01.png" />
+              <AvatarFallback>{getTwoFirstLetters(user.name)}</AvatarFallback>
+            </Avatar>
+            <div className="ml-4 space-y-1">
+              <p className="text-sm font-medium leading-none">{user.name}</p>
+              <p className="text-sm text-muted-foreground">{user.email}</p>
+            </div>
+          </div>
+        ))}
     </div>
   );
 }

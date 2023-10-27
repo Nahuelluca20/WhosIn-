be1f343bea89769ca4c1fd2cc56cbf3a42617eae
Nahuel Luca <nahuelluca20@gmail.com>
2023-10-26 22:56:25 -0300

    add: buttons card event

--- components/cards/events/event-card.tsx
+++ components/cards/events/event-card.tsx
@@ -1,3 +1,4 @@
+import {Button} from "@/components/ui/button";
 import {
   Card,
   CardHeader,
@@ -13,13 +14,27 @@ export default function EventCard() {
       <Card className="min-w-[320px] sm:min-w-[270px] lg:min-w-[320px]">
         <CardHeader>
           <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
+          <CardDescription className="flex gap-1">
+            Integrantes en el grupo: 8{" "}
+            <svg
+              className="h-4 w-4 text-muted-foreground"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+              <circle cx="9" cy="7" r="4" />
+              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+            </svg>
+          </CardDescription>
         </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
+
         <CardFooter>
-          <p>Card Footer</p>
+          <Button className="h-8 w-26 px-2">Crear evento</Button>
         </CardFooter>
       </Card>
     </>
